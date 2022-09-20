dbcab09cbffee1984644c7e5b58a41454d6388ac
erobert <etiennerobert33@gmail.com>
2022-09-20 12:47:31 +0300

    Add auth error handling

--- src/LogIn.tsx
+++ src/LogIn.tsx
@@ -1,36 +1,49 @@
-import { useState } from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useCallback, useState } from 'react';
+import { AuthError, signInWithEmailAndPassword } from 'firebase/auth';
 
 import { auth } from './firebase';
 
 import './LogIn.css';
 
-const signIn = (email: string, password: string) =>
-  signInWithEmailAndPassword(auth, email, password);
-
 const LogIn = () => {
   const [password, setPassword] = useState('');
+  const [error, setError] = useState<string | null>(null);
+
+  const signIn = useCallback(async () => {
+    try {
+      await signInWithEmailAndPassword(
+        auth,
+        'marine.diot@doctolib.com',
+        password
+      );
+    } catch (error) {
+      setError((error as AuthError).message);
+    }
+  }, [password]);
 
   return (
-    <form
-      className="LogIn__Form"
-      onSubmit={(event) => {
-        event.preventDefault();
-        if (password === '') return;
-        signIn('marine.diot@doctolib.com', password);
-      }}
-    >
-      <input
-        className="LogIn__Element"
-        value={password}
-        type="password"
-        placeholder="Password"
-        onChange={(event) => setPassword(event.target.value)}
-      />
-      <button className="LogIn__Element LogIn__Button" type="submit">
-        Unlock
-      </button>
-    </form>
+    <>
+      {error}
+      <form
+        className="LogIn__Form"
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (password === '') return;
+          signIn();
+        }}
+      >
+        <input
+          className="LogIn__Element"
+          value={password}
+          type="password"
+          placeholder="Password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button className="LogIn__Element LogIn__Button" type="submit">
+          Unlock
+        </button>
+      </form>
+    </>
   );
 };
 
